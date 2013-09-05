cf07a115cd19f3e133673b09c9b200ea514b1e64
Barry Shapira <barry.shapira@gmail.com>
2013-09-05 23:42:28 +0300

    Added comment.

--- algorithms/JobScheduling.js
+++ algorithms/JobScheduling.js
@@ -1,5 +1,6 @@
 // Take an array of job objects: [{weight: 0, length: 1}, {weight: 2, length: 4}, {weight: 3, length: 7}, ...]
-// Return the array sorted by descending weight/length.  This greedy algorithm will always produce the optimal ordering.
+// Return the array sorted by descending weight/length.
+// This greedy algorithm will always produce the optimal ordering to minimize the sum of weight times job completion time.
 module.exports = function (jobs) {
     return jobs.sort(function (a, b) {
         return (b.weight / b.length) - (a.weight / a.length);
